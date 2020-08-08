5205ebf3a84c75e0bb14bc51680a1643ed95b6fd
LuizFernando55 <45644952+LuizFernando55@users.noreply.github.com>
2020-08-08 02:50:23 -0300

    Cards mod style; materialize-css

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+
+import M from 'materialize-css'
 import './App.css';
 
 import Routes from './routes'
 
 function App() {
+
+  M.AutoInit();
+
   return (
     <>
       <Routes />
